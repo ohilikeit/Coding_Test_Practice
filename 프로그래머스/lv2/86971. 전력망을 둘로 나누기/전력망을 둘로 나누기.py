38bfcebfa730de929771d4979a4cb6aee16e73bb
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-12 19:17:17 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 14.44 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,36 @@
+import copy
+from collections import Counter
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+    
+def solution(n, wires):
+    answer = int(1e9)
+    
+    for i in wires:
+        tmp = wires[:]
+        tmp.remove(i)
+        parent = [i for i in range(n + 1)]
+        
+        for a, b in tmp:
+            if find_parent(parent, a) == find_parent(parent, b):
+                continue
+            union_parent(parent, a, b)
+        uf = []
+        for i in range(1, n+1):
+            uf.append(find_parent(parent, i))
+        
+        a = list(Counter(uf).values())
+        answer = min(answer, abs(a[0] - a[1]))
+        
+    return answer
