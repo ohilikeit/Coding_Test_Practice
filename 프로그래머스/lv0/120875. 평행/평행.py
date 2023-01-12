17c87341661c1d2d1a09f6d24a2bff0aecb06526
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-12 19:15:08 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -0,0 +1,21 @@
+def solution(dots):
+    gd_1 = (dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0])
+    gd_2 = (dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0])
+    if gd_1 == gd_2:
+        return 1
+    
+    gd_1 = (dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0])
+    gd_2 = (dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0])
+    if gd_1 == gd_2:
+        return 1
+    
+    gd_1 = (dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0])
+    gd_2 = (dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0])
+    if gd_1 == gd_2:
+        return 1
+    
+    
+    # elif dots[0][1] - gd_1*dots[0][0] == dots[1][1] - gd_2*dots[1][0]:
+    #     return 1
+    return 0
+
