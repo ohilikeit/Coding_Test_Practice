0117ebecd59e7f1d14f17d5372c24e852d0ddaaf
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-12 19:16:20 +0900

    [level 3] Title: 외벽 점검, Time: 393.58 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/60062. 외벽 점검/외벽 점검.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+def solution(n, weak, dist):
+    length = len(weak)
+    for i in range(length):
+        weak.append(weak[i] + n)
+    answer = len(weak) + 1
+    for start in range(length):
+        for friends in list(permutations(dist, len(dist))):
+            cnt = 1
+            position = weak[start] + friends[cnt - 1]
+            # 시작 지점부터 모든 취약지점 확인
+            for idx in range(start, start + length):
+                if position < weak[idx]:
+                    cnt += 1
+                    if cnt > len(dist):
+                        break
+                    position = weak[idx] + friends[cnt - 1]
+            answer = min(answer, cnt)
+    if answer > len(dist):
+        return -1
+    return answer
