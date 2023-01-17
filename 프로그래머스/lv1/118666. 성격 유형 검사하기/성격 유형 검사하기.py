0a2540e17d8029af42c1cfacb62a47402a3dcfdc
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-17 23:56:48 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.40 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,24 @@
+def solution(survey, choices):
+    
+    dic = {'R' : 0, 'T' : 0, 
+           'C' : 0, 'F' : 0,
+           'J' : 0, 'M' : 0, 
+           'A' : 0, 'N' : 0}
+    
+    for i in range(len(choices)):
+        x, y = survey[i]
+        num = choices[i]
+        if num in [1,2,3]:
+            dic[x] += 4 - num
+        elif num in [5,6,7]:
+            dic[y] += num - 4
+        else:
+            continue
+    
+    answer = ''
+    answer += 'R' if dic['R'] >= dic['T'] else 'T'
+    answer += 'C' if dic['C'] >= dic['F'] else 'F'
+    answer += 'J' if dic['J'] >= dic['M'] else 'M'
+    answer += 'A' if dic['A'] >= dic['N'] else 'N'
+
+    return answer
