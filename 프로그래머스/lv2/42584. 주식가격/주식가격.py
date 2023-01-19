ac188f78dc431e5a5665e0126e8d22d6e0842a30
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-19 19:42:21 +0900

    [level 2] Title: 주식가격, Time: 34.98 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    q = deque(prices)
+    
+    while q:
+        idx = q.popleft()
+        ans = 0
+        for i in q:
+            ans += 1
+            if i < idx:
+                break
+        answer.append(ans)   
+    return answer
