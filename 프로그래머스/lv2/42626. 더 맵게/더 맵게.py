def7231c8bf1bff0ab7a1f9f70e22a8b6703fba6
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-19 20:10:26 +0900

    [level 2] Title: 더 맵게, Time: 1604.48 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -3,18 +3,15 @@ import heapq
 def solution(scoville, K):
     answer = 0
     scoville.sort()
-    while scoville[0]< K:
+    
+    while scoville[0] < K:
         if len(scoville) <= 1:
-            answer = -1
-            break
-            
-        a = heapq.heappop(scoville)
-        b = heapq.heappop(scoville)
-        if a < K:
+            return -1
+        else:
+            a = heapq.heappop(scoville)
+            b = heapq.heappop(scoville)
             new = a + 2 * b
             heapq.heappush(scoville, new)
             answer += 1
-        else:
-            break
-        
+
     return answer
