d4ea24a8ab16b5b06edaa9e5cc80d1ef0460fc69
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-19 18:33:15 +0900

    [level 2] Title: 프린터, Time: 0.18 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(priorities, location):
+    q = deque()
+    for i, cost in enumerate(priorities):
+        q.append((cost, i))
+        
+    answer = 0
+    while len(q):
+        j = q.popleft()
+        
+        if q and max(q)[0] > j[0]:
+            q.append(j)
+        else:
+            answer += 1
+            if j[1] == location:
+                break
+    return answer
