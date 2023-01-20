32a9f6f471ab411a9ced2292ceba5646c3b91eb5
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-20 11:42:26 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,12 +1,8 @@
-# [9,9,9,9,9,8]
-# [0,1,2,3,4,5]
-
 def solution(citations):
     if list(set(citations)) == [0]:
         return 0
     if len(citations) == 1:
         return 1
-    
     
     citations.sort(reverse=True)
     answer = len(citations)
