b715f6aace4ee2ec3099935b34c4995ec3f2a5ec
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-21 23:31:32 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,25 +1,25 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
 def solution(n, costs):
-
-    def find_parent(parent, n):
-        if parent[n] != n:
-            parent[n] = find_parent(parent, parent[n])
-        return parent[n]
-    
-    def union_parent(parent, a, b):
-        a = find_parent(parent, a)
-        b = find_parent(parent, b)
-        if a > b:
-            parent[a] = b
-        else:
-            parent[b] = a
-    
-    answer = 0 
-    costs.sort(key=lambda x:x[2])  # 비용 기준으로 오름차순 정렬
+    answer = 0
     parent = [i for i in range(n)]
-
-    for node_a, node_b, cost in costs:
-        if find_parent(parent, node_a) != find_parent(parent, node_b):
-            union_parent(parent, node_a, node_b)
+    costs.sort(key = lambda x: x[2])
+    
+    for i in costs:
+        x, y, cost = i
+        if find_parent(parent, x) != find_parent(parent, y):
+            union_parent(parent, x, y)
             answer += cost
-
+    
     return answer
