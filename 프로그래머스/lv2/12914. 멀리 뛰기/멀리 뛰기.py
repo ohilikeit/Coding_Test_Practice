bb91c972c4dcf5d8a2af6bf17c77e27be35e2433
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-24 14:49:08 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.30 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,10 +1,10 @@
 def solution(n):
+    answer = 0
     if n == 1:
-        return 1 % 1234567
-    elif n == 2:
-        return 2 % 1234567
-    else:
-        a,b = 1,2
-        for i in range(n-2):
-            a, b  = b, a+b
-        return b % 1234567
+        return 1
+    dp = [0] * (n+1)
+    dp[1], dp[2] = 1, 2
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+        
+    return dp[n] 
