50163ba119355f51ff41c64eab9d8ce2381342a6
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-26 09:41:46 +0900

    [level 3] Title: N으로 표현, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -1,20 +1,19 @@
-def solution(NUM, number):
-    arr = [[]] + [[int(str(NUM) * i)] for i in range(1,9)]
-
+def solution(N, number):
+    arr = [[]] + [[int(str(N) * i)] for i in range(1, 9)]
     if [number] in arr:
         return arr.index([number])
-
+    
     for i in range(2, 9):
         for j in range(1, i):
             for a in arr[j]:
                 for b in arr[i-j]:
                     arr[i].append(a + b)
-                    arr[i].append(a * b)
                     arr[i].append(a - b)
-                    if 0 != b:
-                        arr[i].append(a // b)
+                    arr[i].append(a * b)
+                    if b != 0:
+                        arr[i].append(a / b)
         if number in arr[i]:
-            return i 
+            return i
         arr[i] = list(set(arr[i]))
-
+        
     return -1
