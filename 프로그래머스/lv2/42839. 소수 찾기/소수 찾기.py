feb7c5bc3da16ba0bce538aa8c3ce1300164269a
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-26 15:26:47 +0900

    [level 2] Title: 소수 찾기, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,35 +1,21 @@
 from itertools import permutations
+
 def solution(numbers):
-    # # 만들 수 있는 모든 경우의 수 구하기 
-    # lst = [i for i in numbers]
-    # total = lst[:]
-    # for i in range(2, len(lst)+1):
-    #     a = list(permutations(lst, i))
-    #     b = []
-    #     for j in a:
-    #         b.append(''.join(j).lstrip('0'))
-    #     b = list(set(b))
-    #     total += b
-    # total = list(set([int(i) for i in total]))
-    
-    
-    answer = []                                   
-    nums = [n for n in numbers]                 
-    per = []                                      
-    for i in range(1, len(numbers)+1):            
-        per += list(permutations(nums, i))       
+    lst = list(numbers)
+    per = []
+    for i in range(1, len(lst)+1):
+        per += list(permutations(lst, i))
     total = [int(("").join(p)) for p in per]
-    # 소수 인지 확인 
-    ans = []
-    for num in total:
-        if num < 2:
-            continue
-        check = True
-        for i in range(2, int(num**(1/2)) + 1):
-            if num % i == 0:
-                check = False
-                break
-        if check:
-            ans.append(num)
+    total = list(set(total))
     
-    return len(set(ans))
+    answer = []
+    for i in total:
+        if i >= 2:
+            ans = True
+            for j in range(2, int(i**0.5)+1):
+                if i % j == 0:
+                    ans = False
+                    break
+            answer.append(ans)
+            
+    return answer.count(True)
