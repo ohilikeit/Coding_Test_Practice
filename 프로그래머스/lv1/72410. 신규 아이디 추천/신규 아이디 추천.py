bcb301f28ba97c297c6cc69e13330d3e0a011a36
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-01-26 14:20:34 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.23 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,30 +1,12 @@
 import re
 
 def solution(new_id):
-    answer = ''
-    
-    new_id = new_id.lower()
-    
-    new_id = re.sub(r'[^a-z0-9-_\.]', '', new_id)
-    
-    new_id = re.sub(r'\.{2,}', '.', new_id)
-    
-    if new_id[0] == '.':
-        new_id = new_id[1:]
-    if new_id[-1:] == '.':
-        new_id = new_id[:-1]
-        
-    if len(new_id) == 0:
-        new_id = 'a'
-        
-    if len(new_id) >= 16:
-        new_id = new_id[:15]
-        if new_id[-1] == '.':
-            new_id = new_id[:14]
-            
-    if len(new_id) <= 2:
-        idx = new_id[-1]
-        while len(new_id) < 3:
-            new_id += idx
-    
-    return new_id
+    st = new_id
+    st = st.lower()
+    st = re.sub('[^a-z0-9\-_.]', '', st)
+    st = re.sub('\.+', '.', st)
+    st = re.sub('^\.|\.$', '', st)
+    st = 'a' if len(st) == 0 else st[:15]
+    st = re.sub('^[.]|[.]$', '', st)
+    st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
+    return st
