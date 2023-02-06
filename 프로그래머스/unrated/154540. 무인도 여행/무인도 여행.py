a88c1c3e2ff344b0cd8067dfecb0926b1df7fd8b
jihwanyoon <37128004+ohilikeit@users.noreply.github.com>
2023-02-06 23:21:49 +0900

    [unrated] Title: 무인도 여행, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -7,7 +7,6 @@ def solution(maps):
     dy = [0, 1, 0, -1]
     visited = [[False] * m for _ in range(n)]
     
-    
     for i in range(n):
         for j in range(m):
             if maps[i][j] == 'X':
