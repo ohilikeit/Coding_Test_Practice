4b40586127c4f9bc70e78ecebc6ea188e2e18247
jihwanyoon <yjh091500@naver.com>
2023-03-23 15:41:05 +0900

    [level 1] Title: 모의고사, Time: 2.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -3,10 +3,10 @@ def solution(answers):
     a = [1,2,3,4,5] * (len(answers) // 5 + 1)
     b = [2,1,2,3,2,4,2,5] * (len(answers) // 8 + 1)
     c = [3,3,1,1,2,2,4,4,5,5] * (len(answers) // 10 + 1)
-    
     score1 = 0
     score2 = 0
     score3 = 0
+    
     for i in range(len(answers)):
         if a[i] == answers[i]:
             score1 += 1
