7408486cfbcfb73f8d658c93d6c1b2b83eb6ae33
jihwanyoon <yjh091500@naver.com>
2023-04-06 14:18:28 +0900

    [level 2] Title: 전화번호 목록, Time: 90.00 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,11 @@
 def solution(phone_book):
     answer = True
+    # 정렬 --> str 안에 숫자들은 말그대로 숫자 순서대로 정렬된다. 
+    # ["119", "97674223", "1195524421"] --> ["119", "1195524421", "97674223"]
     phone_book.sort()
+    
+    # for문이 돌아가면서 앞의 문자가 바로 뒤 문자의 접두사인지를 startswith으로 확인하고
+    # 발견하면 바로 False를 뱉어준다! 
     for i in range(len(phone_book)-1):
         if phone_book[i+1].startswith(phone_book[i]):
             answer = False
