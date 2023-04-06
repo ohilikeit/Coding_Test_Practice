2a289d221abc5a58994ac10d50b43cbd7e6f3e6d
jihwanyoon <yjh091500@naver.com>
2023-04-06 14:07:40 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.py
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -1,13 +1,10 @@
+from collections import defaultdict
+
 def solution(clothes):
-    hashy = {}
-    for idx in clothes:
-        a, b = idx
-        if b not in hashy.keys():
-            hashy[b] = []
-            hashy[b].append(a)
-        else:
-            hashy[b].append(a)
-            
+    hashy = defaultdict(list)
+    for a, b in clothes:
+        hashy[b].append(a)
+        
     answer = 1
     for val in list(hashy.values()):
         answer *= (1+len(val))
