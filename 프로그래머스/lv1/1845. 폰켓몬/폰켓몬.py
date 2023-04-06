711fff794c5e24bb088bf90138d5afd01cd9b301
jihwanyoon <yjh091500@naver.com>
2023-04-06 16:24:12 +0900

    [level 1] Title: 폰켓몬, Time: 0.36 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,4 @@
 def solution(nums):
-    answer = 0
-    lst = list(set(nums))
-    if len(lst) >= len(nums) // 2:
-        answer = len(nums) // 2
-    else:
-        answer = len(lst)
-    
-    return answer
-
+    n = len(nums)/2
+    nums = len(set(nums))
+    return min(n, nums)
