84f3f887af41f3627a7e7d542e330b47fef2c98e
jihwanyoon <yjh091500@naver.com>
2023-05-10 15:15:09 +0900

    [level 3] Title: 순위, Time: 97.56 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.py
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -6,7 +6,7 @@ def solution(n, results):
     for k in range(1, n+1):
         for a in range(1, n+1):
             for b in range(1, n+1):
-                if graph[a][k] ==1 and graph[k][b] == 1:
+                if graph[a][k] == 1 and graph[k][b] == 1:
                     graph[a][b] = 1
     lst = [0 for _ in range(n+1)]
     for i in range(1, n+1):
