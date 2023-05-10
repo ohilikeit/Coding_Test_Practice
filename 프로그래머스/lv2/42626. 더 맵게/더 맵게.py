f858b3c668546353c81d59667053879b7a678baa
jihwanyoon <yjh091500@naver.com>
2023-05-10 13:54:51 +0900

    [level 2] Title: 더 맵게, Time: 1682.25 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -3,15 +3,13 @@ import heapq
 def solution(scoville, K):
     answer = 0
     scoville.sort()
-    
     while scoville[0] < K:
         if len(scoville) <= 1:
-            return -1
-        else:
-            a = heapq.heappop(scoville)
-            b = heapq.heappop(scoville)
-            new = a + 2 * b
-            heapq.heappush(scoville, new)
-            answer += 1
-
+            return - 1
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        new = a + b * 2
+        heapq.heappush(scoville, new)
+        answer += 1
+        
     return answer
