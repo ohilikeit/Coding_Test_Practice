b9825e0a939d69a72731720734db343c238dc5fd
jihwanyoon <yjh091500@naver.com>
2023-05-17 14:59:32 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -5,13 +5,13 @@ def solution(tickets):
     for i in graph.keys():
         graph[i].sort(reverse=True)
     q = ['ICN']
-    path = []
+    answer = []
     while q:
         top = q[-1]
         if top not in graph or len(graph[top]) == 0:
-            path.append(q.pop())
+            answer.append(q.pop())
         else:
             q.append(graph[top][-1])
             graph[top] = graph[top][:-1]
             
-    return path[::-1]
+    return answer[::-1]
