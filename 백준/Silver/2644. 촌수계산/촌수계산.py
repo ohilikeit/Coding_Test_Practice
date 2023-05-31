a765d3151b916c2cfb3889682e464cf6ea551e0b
jihwanyoon <yjh091500@naver.com>
2023-05-31 15:45:21 +0900

    [Silver II] Title: 촌수계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,31 @@
+N = int(input())
+A, B = map(int, input().split())
+M = int(input())
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+result = []
+
+for _ in range(M):
+  x, y = map(int, input().split())  
+  graph[x].append(y)
+  graph[y].append(x)
+
+def dfs(v, num):
+  stack = [(v, num)]
+  while stack:
+    v, num = stack.pop()
+    num += 1
+    visited[v] = True
+
+    if v == B:
+      result.append(num)
+
+    for i in graph[v]:
+      if not visited[i]:
+        stack.append((i, num))
+
+dfs(A, 0)
+if len(result) == 0:
+  print(-1)
+else:
+  print(result[0]-1)
