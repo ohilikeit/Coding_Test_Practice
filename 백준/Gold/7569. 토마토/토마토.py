3202ee132c91ec13c519d5439a966f1e383b08fb
jihwanyoon <yjh091500@naver.com>
2023-06-06 22:19:40 +0900

    [Gold V] Title: 토마토, Time: 2468 ms, Memory: 48532 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,37 @@
+from collections import deque
+M, N, H = map(int, input().split())
+graph = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+
+q = deque()
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
+
+def bfs():
+    while q:
+        z, x, y = q.popleft()
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
+            if (-1 < nx < N) and (-1 < ny < M) and (-1 < nz < H):
+                if graph[nz][nx][ny] == 0:
+                    graph[nz][nx][ny] = graph[z][x][y] + 1
+                    q.append((nz, nx, ny))
+
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if graph[i][j][k] == 1:
+                q.append((i,j,k))
+
+bfs()
+res = 0
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if graph[i][j][k] == 0:
+                print(-1)
+                exit()
+            res = max(res, graph[i][j][k])
+print(res-1)
