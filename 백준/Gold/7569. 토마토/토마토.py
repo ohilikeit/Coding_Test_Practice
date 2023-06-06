c22c80953923121dfe1b8fcb62e16614e2b5c484
jihwanyoon <yjh091500@naver.com>
2023-06-06 22:21:22 +0900

    [Gold V] Title: 토마토, Time: 2104 ms, Memory: 48532 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,4 +1,6 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 M, N, H = map(int, input().split())
 graph = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 
