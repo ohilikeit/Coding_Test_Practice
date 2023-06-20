6257cd794d96b961db4b6abcbc41176235f654da
jihwanyoon <yjh091500@naver.com>
2023-06-20 22:29:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,7 +1,7 @@
 def solution(array, commands):
     answer = []
-    for idx in commands:
-        i, j, k = idx
+    for lst in commands:
+        i, j, k = lst[0], lst[1], lst[2]
         answer.append(sorted(array[i-1:j])[k-1])
-        
+    
     return answer
