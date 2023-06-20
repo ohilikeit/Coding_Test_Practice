765863155acc0caad47c998b3deccedfadc0ab85
jihwanyoon <yjh091500@naver.com>
2023-06-20 22:40:14 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,10 +1,11 @@
 def solution(numbers):
-    if list(set(numbers)) == [0]:
-        return "0"
     answer = ''
-    numbers = [(4 * str(i)) for i in numbers]
-    numbers.sort(reverse=True, key = lambda x: [x[0],x[1],x[2],x[3]])
-    for i in range(len(numbers)):
-        answer += numbers[i][:len(numbers[i])//4]  
-        
+    if list(set(numbers)) == [0]:
+        return '0'
+    new_numbers = [4 * str(i) for i in numbers]
+    new_numbers.sort(reverse=True, key = lambda x: [x[0], x[1], x[2], x[3]])
+    
+    for i in new_numbers:
+        answer += str(i[:len(i) // 4])
+    
     return answer
