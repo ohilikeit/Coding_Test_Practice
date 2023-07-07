979c7011f60042dd5b8b0e655b695f10d98e8888
jihwanyoon <yjh091500@naver.com>
2023-07-07 13:26:20 +0900

    [level 3] Title: 등굣길, Time: 2.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -4,9 +4,9 @@ def solution(m, n, puddles):
     graph[1][1] = 1
     for i in range(1, n+1):
         for j in range(1, m+1):
-            if [j, i] in puddles or [i, j] == [1,1]:
+            if [i,j] == [1,1] or [j,i] in puddles:
                 continue
             else:
                 graph[i][j] = graph[i-1][j] + graph[i][j-1]
     
-    return graph[-1][-1] % 1000000007
+    return graph[n][m] % 1000000007
