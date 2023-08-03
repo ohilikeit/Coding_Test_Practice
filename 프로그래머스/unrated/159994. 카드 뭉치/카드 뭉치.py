806453952b075f552952d8c49008e695bb4658f0
jihwanyoon <yjh091500@naver.com>
2023-08-03 10:00:09 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,18 +1,15 @@
 def solution(cards1, cards2, goal):
-    cards1 = cards1[::-1]
-    cards2 = cards2[::-1]
-    goal = goal[::-1]
-    while goal:
-        ans = goal.pop()
-        if cards1:
-            a = cards1.pop()
-        if cards2:
-            b = cards2.pop()
-        if a and ans == a:
-            cards2.append(b)
-        elif b and ans == b:
-            cards1.append(a)
-        else:
-            return "No"        
+    cards1 = cards1[::-1]  # Reversing the order to make it work like a stack
+    cards2 = cards2[::-1]  # Reversing the order to make it work like a stack
+    goal = goal[::-1]  # Reversing the order to make it work like a stack
     
-    return 'Yes'
+    while goal:
+        if cards1 and cards1[-1] == goal[-1]:  # Check the top card of the first stack
+            cards1.pop()
+            goal.pop()
+        elif cards2 and cards2[-1] == goal[-1]:  # Check the top card of the second stack
+            cards2.pop()
+            goal.pop()
+        else:
+            return "No"
+    return "Yes"  # If all cards in the goal stack are checked, return "Yes"
