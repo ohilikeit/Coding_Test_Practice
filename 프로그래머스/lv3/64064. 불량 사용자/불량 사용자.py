f54937f5abce558da0f7eb304b917c21d3482585
jihwanyoon <yjh091500@naver.com>
2023-08-11 09:15:08 +0900

    [level 3] Title: 불량 사용자, Time: 8.94 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -1,24 +1,13 @@
 import re
 from itertools import permutations
 
-
-
 def solution(user_id, banned_id):
     n = len(banned_id)
     banned_id = [i.replace("*", ".") for i in banned_id]
-    answer = []
+    answer = set()
 
-    for i in permutations(user_id, n):
-        lst = list(i)
-        flag = True
-        for j in range(n):
-            if re.match(banned_id[j], lst[j]) and (len(banned_id[j]) == len(lst[j])) :
-                continue 
-            else:
-                flag = False
-                break
-        if flag:
-            if sorted(lst) not in answer:
-                answer.append(sorted(lst))
-
-    return len(answer) 
+    for perm in permutations(user_id, n):
+        if all(re.fullmatch(banned_id[i], perm[i]) for i in range(n)):
+            answer.add(tuple(sorted(perm)))
+            
+    return len(answer)
