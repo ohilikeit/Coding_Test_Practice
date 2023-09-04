f47497fb0f22370f49a25039c4876b46eeda0dd7
jihwanyoon <yjh091500@naver.com>
2023-09-04 23:51:00 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.18 ms, Memory: 9.91 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -5,9 +5,9 @@ def solution(weights):
 
     for i in range(100, 1001):
         if counter[i] > 0:
-            answer += counter[i] * (counter[i] - 1) // 2 # 같은 수끼리
-            answer += counter[i] * counter[i * 3 / 2]
-            answer += counter[i] * counter[i * 2]
-            answer += counter[i] * counter[i * 4 / 3]
+            answer += counter[i] * (counter[i] - 1) // 2 # 1:1
+            answer += counter[i] * counter[i * 3 / 2]    # 2:3
+            answer += counter[i] * counter[i * 2]        # 2:4 
+            answer += counter[i] * counter[i * 4 / 3]    # 3:4
 
     return answer
