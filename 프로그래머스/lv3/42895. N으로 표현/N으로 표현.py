287bd83c77fe5bc0a0041a550bc1b9e75f3217a9
jihwanyoon <yjh091500@naver.com>
2023-09-08 10:48:42 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -1,19 +1,25 @@
 def solution(N, number):
-    arr = [[]] + [[int(str(N) * i)] for i in range(1, 9)]
-    if [number] in arr:
-        return arr.index([number])
-    
+    if N == number:
+        return 1
+
+    dp = [set() for _ in range(9)]
+    dp[1].add(N)
+
     for i in range(2, 9):
+        dp[i].add(int(str(N) * i))
         for j in range(1, i):
-            for a in arr[j]:
-                for b in arr[i-j]:
-                    arr[i].append(a + b)
-                    arr[i].append(a - b)
-                    arr[i].append(a * b)
+            for a in dp[j]:
+                for b in dp[i-j]:
+                    dp[i].add(a + b)
+                    dp[i].add(a - b)
+                    dp[i].add(b - a)
+                    dp[i].add(a * b)
                     if b != 0:
-                        arr[i].append(a / b)
-        if number in arr[i]:
+                        dp[i].add(a // b)
+                    if a != 0:
+                        dp[i].add(b // a)
+
+        if number in dp[i]:
             return i
-        arr[i] = list(set(arr[i]))
-        
+    
     return -1
