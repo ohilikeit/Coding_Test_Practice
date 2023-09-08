20ef1be931980518ccc0f49220789e1cb8b9336d
jihwanyoon <yjh091500@naver.com>
2023-09-08 23:24:29 +0900

    [level 3] Title: 가장 먼 노드, Time: 72.34 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,17 +1,23 @@
 import heapq
 
 def solution(n, edge):
+    '''heapq를 사용한 다익스트라 최단거리 알고리즘'''
     answer = 0
     INF = int(1e9)
     distance = [INF] * (n+1)
+    
+    # 노드정보 리스트
     graph = [[] for _ in range(n+1)]
+    
+    # a번 노드에서 b번 노드까지 거리는 1, 간선은 양방향
     for a, b in edge:
         graph[a].append((b,1))
         graph[b].append((a,1))
     q = []
+    # 시작 노드 설정
     start = 1
     heapq.heappush(q, (0, start))
-    distance[start] = 0
+    distance[start] = 0 # 시작 노드까지 거리는 0
     while q:
         dist, now = heapq.heappop(q)
         if distance[now] < dist:
