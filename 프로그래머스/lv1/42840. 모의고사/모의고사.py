e74b4913c351b58d21500a5bdc8953676f3b7c9e
jihwanyoon <yjh091500@naver.com>
2023-09-08 19:12:24 +0900

    [level 1] Title: 모의고사, Time: 2.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,27 +1,24 @@
 def solution(answers):
     answer = []
-    a = [1,2,3,4,5] * (len(answers) // 5 + 1)
-    b = [2,1,2,3,2,4,2,5] * (len(answers) // 8 + 1)
-    c = [3,3,1,1,2,2,4,4,5,5] * (len(answers) // 10 + 1)
-    score1 = 0
-    score2 = 0
-    score3 = 0
+    n = len(answers)
+    lst_1 = [1,2,3,4,5] * (n // 5 + 1)
+    lst_2 = [2,1,2,3,2,4,2,5] * (n // 8 + 1)
+    lst_3 = [3,3,1,1,2,2,4,4,5,5] * (n // 10 + 1)
     
-    for i in range(len(answers)):
-        if a[i] == answers[i]:
-            score1 += 1
-        if b[i] == answers[i]:
-            score2 += 1
-        if c[i] == answers[i]:
-            score3 += 1
+    ans_1 = 0
+    ans_2 = 0
+    ans_3 = 0
     
-    lst = [(score1, 1), (score2, 2), (score3, 3)]
-    answer = []
-    for i in lst:
-        score, num = i
-        if score == max(lst)[0]:
-            answer.append(num)
+    for i in range(n):
+        if lst_1[i] == answers[i]:
+            ans_1 += 1
+        if lst_2[i] == answers[i]:
+            ans_2 += 1
+        if lst_3[i] == answers[i]:
+            ans_3 += 1
     
+    maxi = max(ans_1, ans_2, ans_3)
+    answer = [(ans_1, 1), (ans_2, 2), (ans_3, 3)]
+    ans = [i[1] for i in sorted(answer) if i[0] == maxi]
     
-    return sorted(answer)
-    
+    return ans
