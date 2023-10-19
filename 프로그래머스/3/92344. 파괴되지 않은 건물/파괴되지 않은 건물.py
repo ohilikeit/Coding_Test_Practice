b3d2461c1261ce87496dc15826c2a1e392a59f1a
jihwanyoon <yjh091500@naver.com>
2023-10-19 18:02:04 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 1307.48 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,20 +1,30 @@
 def solution(board, skill):
-    MAX_R, MAX_C = len(board), len(board[0])
-    imos = [[0] * (MAX_C + 1) for _ in range(MAX_R + 1)]
+    answer = 0
+    R, C = len(board), len(board[0])
+    imos = [[0] * (C+1) for _ in range(R+1)]
+    
+    for typ, r1, c1, r2, c2, degree in skill:
+        attack_heal = (-1) * degree if typ == 1 else degree
+        
+        # imos 누적합, 4개의 모서리 중 처음과 끝을 제외한 중간 두 개는 마이너스
+        imos[r1][c1] += attack_heal
+        imos[r1][c2+1] -= attack_heal
+        imos[r2+1][c1] -= attack_heal
+        imos[r2+1][c2+1] += attack_heal
+        
+    # 누적합 계산하기
+    for i in range(R+1):
+        for j in range(1, C+1):
+            imos[i][j] += imos[i][j-1]
+    for j in range(C+1):
+        for i in range(1, R+1):
+            imos[i][j] += imos[i-1][j]
+    
+    # 최종 계산 후 갯수 세기
+    for i in range(R):
+        for j in range(C):
+            board[i][j] += imos[i][j]
+            if board[i][j] > 0:
+                answer += 1
 
-    for type, r1, c1, r2, c2, degree in skill:
-        attack_or_heal = (-1) * degree if type == 1 else degree
-        imos[r1][c1] += attack_or_heal
-        imos[r2 + 1][c1] -= attack_or_heal
-        imos[r1][c2 + 1] -= attack_or_heal
-        imos[r2 + 1][c2 + 1] += attack_or_heal
-
-    for r in range(MAX_R + 1):
-        for c in range(1, MAX_C + 1):
-            imos[r][c] += imos[r][c - 1]
-
-    for c in range(MAX_C + 1):
-        for r in range(1, MAX_R + 1):
-            imos[r][c] += imos[r - 1][c]
-
-    return sum([1 if 0 < board[r][c] + imos[r][c] else 0 for r in range(MAX_R) for c in range(MAX_C)])
+    return answer
