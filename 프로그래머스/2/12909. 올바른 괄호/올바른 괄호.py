2f65d2e6220bde09a2f1ce64cda470229e9db62e
jihwanyoon <yjh091500@naver.com>
2023-10-20 16:55:23 +0900

    [level 2] Title: 올바른 괄호, Time: 7.11 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if not stack:
+                return False
+            stack.pop()
+
+    return not stack
