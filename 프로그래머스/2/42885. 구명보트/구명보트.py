f14aaa362788b6e764f96b54261b6c48cb0afbc9
jihwanyoon <yjh091500@naver.com>
2023-10-21 11:41:49 +0900

    [level 2] Title: 구명보트, Time: 7.85 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,12 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    s, e = 0, len(people)-1
+    
+    while s < e:
+        if people[s] + people[e] <= limit:
+            s += 1
+            answer += 1
+        e -= 1    
+    
+    return len(people) - answer
